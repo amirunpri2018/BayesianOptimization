0de216884fb383e85e3a4ddc313660a2fa4b047f
fmfn <fmfnogueira@gmail.com>
2018-11-24 13:07:00 -0600

    wip - updating observer tests

--- tests/test_observer.py
+++ tests/test_observer.py
@@ -1,42 +1,53 @@
-import unittest
-from bayes_opt.observer import Observable
+import pytest
+from bayes_opt.bayesian_optimization import Observable
 
 
-class TestObserver():
+EVENTS = ["a", "b", "c"]
+
+
+class SimpleObserver():
     def __init__(self):
         self.counter = 0
 
     def update(self, event, instance):
         self.counter += 1
 
-class TestObserverPattern(unittest.TestCase):
-    def setUp(self):
-        events = ['a', 'b']
-        self.observable = Observable(events)
-        self.observer = TestObserver()
 
-    def test_get_subscribers(self):
-        self.observable.register('a', self.observer)
-        self.assertTrue(self.observer in self.observable.get_subscribers('a'))
-        self.assertTrue(len(self.observable.get_subscribers('a').keys()) == 1)
-        self.assertTrue(len(self.observable.get_subscribers('b').keys()) == 0)
+def test_get_subscribers():
+    observer = SimpleObserver()
+    observable = Observable(events=EVENTS)
 
-    def test_register(self):
-        self.observable.register('a', self.observer)
-        self.assertTrue(self.observer in self.observable.get_subscribers('a'))
+    observable.subscribe("a", observer)
+    assert observer in observable.get_subscribers('a')
+    assert observer not in observable.get_subscribers('b')
+    assert observer not in observable.get_subscribers('c')
 
-    def test_unregister(self):
-        self.observable.register('a', self.observer)
-        self.observable.unregister('a', self.observer)
-        self.assertTrue(self.observer not in self.observable.get_subscribers('a'))
+    assert len(observable.get_subscribers('a')) == 1)
+    assert len(observable.get_subscribers('b')) == 0)
+    assert len(observable.get_subscribers('c')) == 0)
 
-    def test_dispatch(self):
-        test_observer = TestObserver()
-        self.observable.register('b', test_observer)
-        self.observable.dispatch('b')
-        self.observable.dispatch('b')
+# class TestObserverPattern(unittest.TestCase):
+#     def setUp(self):
+#         events = ['a', 'b']
+#         self.observable = Observable(events)
+#         self.observer = TestObserver()
 
-        self.assertTrue(test_observer.counter == 2)
+#     def test_register(self):
+#         self.observable.register('a', self.observer)
+#         self.assertTrue(self.observer in self.observable.get_subscribers('a'))
+
+#     def test_unregister(self):
+#         self.observable.register('a', self.observer)
+#         self.observable.unregister('a', self.observer)
+#         self.assertTrue(self.observer not in self.observable.get_subscribers('a'))
+
+#     def test_dispatch(self):
+#         test_observer = TestObserver()
+#         self.observable.register('b', test_observer)
+#         self.observable.dispatch('b')
+#         self.observable.dispatch('b')
+
+#         self.assertTrue(test_observer.counter == 2)
 
 
 if __name__ == '__main__':
@@ -44,6 +55,4 @@ if __name__ == '__main__':
     CommandLine:
         python tests/test_observer.py
     """
-    # unittest.main()
-    import pytest
     pytest.main([__file__])
